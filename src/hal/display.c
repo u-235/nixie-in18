c3e326a66712a2eaaa21059d09dbe1598c954d4c
u-235 <nicegorov@yandex.ru>
2018-11-21 13:29:38 +0300

    Рефактроинг - избавление от магии.
    Благодаря bit-helper.h и config.h стало возможным избавиться от
    магических и чисел и неочевидных масок. И хотя в некоторых функциях,
    таких как display_hours, увеличился объем, компилируется это в тот
    же код, что и раньше.

--- src/hal/display.c
+++ src/hal/display.c
@@ -1,9 +1,15 @@
-#include <avr/io.h>
+#include "../bit-helper.h"
+#include "../config.h"
 #include "display.h"
 #include "avr/spi.h"
 #include "avr/pwmi.h"
 
 static uint8_t data[SPI_ARRAY_SIZE];
+
+#define _segment_on(seg)        _array_bit_set(seg)
+#define _segment_off(seg)       _array_bit_clean(seg)
+#define _array_bit_set(r, b)    _bit_set(data[r], b)
+#define _array_bit_clean(r, b)  _bit_clear(data[r], b)
 
 /**
  * Инициализация индикации.
@@ -20,9 +26,10 @@ void display_init()
  */
 void display_clean()
 {
-        unsigned char *p = data;
+        uint8_t *p = data;
+        uint8_t i;
 
-        for (char i = 0; i < SPI_ARRAY_SIZE; i++) {
+        for (i = 0; i < SPI_ARRAY_SIZE; i++) {
                 *p++ = 0;
         }
 }
@@ -52,51 +59,51 @@ void display_flush()
 void display_day(uint8_t d)
 {
         if (d & DISPLAY_MARK_SUNDAY) {
-                data[7] |= 1 << 2;
+                _segment_on(CFG_REG_SUNDAY);
         } else {
-                data[7] &= ~(1 << 2);
+                _segment_off(CFG_REG_SUNDAY);
         }
 
         if (d & DISPLAY_MARK_SATURDAY) {
-                data[6] |= 1 << 5;
+                _segment_on(CFG_REG_SATURDAY);
         } else {
-                data[6] &= ~(1 << 5);
+                _segment_off(CFG_REG_SATURDAY);
         }
 
         if (d & DISPLAY_MARK_FRIDAY) {
-                data[5] |= 1 << 0;
+                _segment_on(CFG_REG_FRIDAY);
         } else {
-                data[5] &= ~(1 << 0);
+                _segment_off(CFG_REG_FRIDAY);
         }
 
         if (d & DISPLAY_MARK_THURSDAY) {
-                data[4] |= 1 << 3;
+                _segment_on(CFG_REG_THURSDAY);
         } else {
-                data[4] &= ~(1 << 3);
+                _segment_off(CFG_REG_THURSDAY);
         }
 
         if (d & DISPLAY_MARK_WEDNESDAY) {
-                data[3] |= 1 << 6;
+                _segment_on(CFG_REG_WEDNESDAY);
         } else {
-                data[3] &= ~(1 << 6);
+                _segment_off(CFG_REG_WEDNESDAY);
         }
 
         if (d & DISPLAY_MARK_TEUSDAY) {
-                data[2] |= 1 << 1;
+                _segment_on(CFG_REG_TEUSDAY);
         } else {
-                data[2] &= ~(1 << 1);
+                _segment_off(CFG_REG_TEUSDAY);
         }
 
         if (d & DISPLAY_MARK_MONDAY) {
-                data[1] |= 1 << 4;
+                _segment_on(CFG_REG_MONDAY);
         } else {
-                data[1] &= ~(1 << 4);
+                _segment_off(CFG_REG_MONDAY);
         }
 
         if (d & DISPLAY_MARK_ALARM) {
-                data[0] |= 1 << 7;
+                _segment_on(CFG_REG_ALARM);
         } else {
-                data[0] &= ~(1 << 7);
+                _segment_off(CFG_REG_ALARM);
         }
 }
 
@@ -108,76 +115,93 @@ void display_day(uint8_t d)
  */
 void display_hours(bcd2_t h)
 {
-        // Стирание минут в массиве.
-        data[2] &= 1 << 0 | 1 << 1; // Бит вторника и разделителя не трогаем
-        data[1] &= 1 << 4; // Бит четверга не трогаем
-        data[0] &= 1 << 7; // Бит будильника не трогаем
+        /* Предварительное стирание сегментов. */
+        _segment_off(CFG_REG_x0xxxx);
+        _segment_off(CFG_REG_x1xxxx);
+        _segment_off(CFG_REG_x2xxxx);
+        _segment_off(CFG_REG_x3xxxx);
+        _segment_off(CFG_REG_x4xxxx);
+        _segment_off(CFG_REG_x5xxxx);
+        _segment_off(CFG_REG_x6xxxx);
+        _segment_off(CFG_REG_x7xxxx);
+        _segment_off(CFG_REG_x8xxxx);
+        _segment_off(CFG_REG_x9xxxx);
+        _segment_off(CFG_REG_0xxxxx);
+        _segment_off(CFG_REG_1xxxxx);
+        _segment_off(CFG_REG_2xxxxx);
+        _segment_off(CFG_REG_3xxxxx);
+        _segment_off(CFG_REG_4xxxxx);
+        _segment_off(CFG_REG_5xxxxx);
+        _segment_off(CFG_REG_6xxxxx);
+        _segment_off(CFG_REG_7xxxxx);
+        _segment_off(CFG_REG_8xxxxx);
+        _segment_off(CFG_REG_9xxxxx);
 
         // Единицы.
         switch (h & 0x0f) {
         case 0:
-                data[1] |= 1 << 0;
+                _segment_on(CFG_REG_x0xxxx);
                 break;
         case 1:
-                data[2] |= 1 << 7;
+                _segment_on(CFG_REG_x1xxxx);
                 break;
         case 2:
-                data[2] |= 1 << 6;
+                _segment_on(CFG_REG_x2xxxx);
                 break;
         case 3:
-                data[2] |= 1 << 4;
+                _segment_on(CFG_REG_x3xxxx);
                 break;
         case 4:
-                data[1] |= 1 << 2;
+                _segment_on(CFG_REG_x4xxxx);
                 break;
         case 5:
-                data[1] |= 1 << 3;
+                _segment_on(CFG_REG_x5xxxx);
                 break;
         case 6:
-                data[2] |= 1 << 2;
+                _segment_on(CFG_REG_x6xxxx);
                 break;
         case 7:
-                data[2] |= 1 << 3;
+                _segment_on(CFG_REG_x7xxxx);
                 break;
         case 8:
-                data[2] |= 1 << 5;
+                _segment_on(CFG_REG_x8xxxx);
                 break;
         case 9:
-                data[1] |= 1 << 1;
+                _segment_on(CFG_REG_x9xxxx);
                 break;
         }
 
         // Десятки.
         switch (h & 0xf0) {
         case 0x00:
-                data[0] |= 1 << 3;
+                _segment_on(CFG_REG_0xxxxx);
                 break;
         case 0x10:
-                data[0] |= 1 << 2;
+                _segment_on(CFG_REG_1xxxxx);
                 break;
         case 0x20:
-                data[0] |= 1 << 1;
+                _segment_on(CFG_REG_2xxxxx);
                 break;
         case 0x30:
-                data[1] |= 1 << 7;
+                _segment_on(CFG_REG_3xxxxx);
                 break;
         case 0x40:
-                data[0] |= 1 << 5;
+                _segment_on(CFG_REG_4xxxxx);
                 break;
         case 0x50:
-                data[0] |= 1 << 6;
+                _segment_on(CFG_REG_5xxxxx);
                 break;
         case 0x60:
-                data[1] |= 1 << 5;
+                _segment_on(CFG_REG_6xxxxx);
                 break;
         case 0x70:
-                data[1] |= 1 << 6;
+                _segment_on(CFG_REG_7xxxxx);
                 break;
         case 0x80:
-                data[0] |= 1 << 0;
+                _segment_on(CFG_REG_8xxxxx);
                 break;
         case 0x90:
-                data[0] |= 1 << 4;
+                _segment_on(CFG_REG_9xxxxx);
                 break;
         }
 }
@@ -190,76 +214,93 @@ void display_hours(bcd2_t h)
  */
 void display_minutes(bcd2_t m)
 {
-        // Стирание минут в массиве.
-        data[5] &= 1 << 0; // Бит пятницы не трогаем
-        data[4] &= 1 << 3; // Бит четверга не трогаем
-        data[3] &= 1 << 6 | 1 << 7; // Бит среды и разделителя не трогаем
+        /* Предварительное стирание сегментов. */
+        _segment_off(CFG_REG_xxx0xx);
+        _segment_off(CFG_REG_xxx1xx);
+        _segment_off(CFG_REG_xxx2xx);
+        _segment_off(CFG_REG_xxx3xx);
+        _segment_off(CFG_REG_xxx4xx);
+        _segment_off(CFG_REG_xxx5xx);
+        _segment_off(CFG_REG_xxx6xx);
+        _segment_off(CFG_REG_xxx7xx);
+        _segment_off(CFG_REG_xxx8xx);
+        _segment_off(CFG_REG_xxx9xx);
+        _segment_off(CFG_REG_xx0xxx);
+        _segment_off(CFG_REG_xx1xxx);
+        _segment_off(CFG_REG_xx2xxx);
+        _segment_off(CFG_REG_xx3xxx);
+        _segment_off(CFG_REG_xx4xxx);
+        _segment_off(CFG_REG_xx5xxx);
+        _segment_off(CFG_REG_xx6xxx);
+        _segment_off(CFG_REG_xx7xxx);
+        _segment_off(CFG_REG_xx8xxx);
+        _segment_off(CFG_REG_xx9xxx);
 
         // Единицы.
         switch (m & 0x0f) {
         case 0:
-                data[5] |= 1 << 7;
+                _segment_on(CFG_REG_xxx0xx);
                 break;
         case 1:
-                data[5] |= 1 << 6;
+                _segment_on(CFG_REG_xxx1xx);
                 break;
         case 2:
-                data[5] |= 1 << 5;
+                _segment_on(CFG_REG_xxx2xx);
                 break;
         case 3:
-                data[5] |= 1 << 3;
+                _segment_on(CFG_REG_xxx3xx);
                 break;
         case 4:
-                data[4] |= 1 << 1;
+                _segment_on(CFG_REG_xxx4xx);
                 break;
         case 5:
-                data[4] |= 1 << 2;
+                _segment_on(CFG_REG_xxx5xx);
                 break;
         case 6:
-                data[5] |= 1 << 1;
+                _segment_on(CFG_REG_xxx6xx);
                 break;
         case 7:
-                data[5] |= 1 << 2;
+                _segment_on(CFG_REG_xxx7xx);
                 break;
         case 8:
-                data[5] |= 1 << 4;
+                _segment_on(CFG_REG_xxx8xx);
                 break;
         case 9:
-                data[4] |= 1 << 0;
+                _segment_on(CFG_REG_xxx9xx);
                 break;
         }
 
         // Десятки.
         switch (m & 0xf0) {
         case 0x00:
-                data[3] |= 1 << 2;
+                _segment_on(CFG_REG_xx0xxx);
                 break;
         case 0x10:
-                data[3] |= 1 << 1;
+                _segment_on(CFG_REG_xx1xxx);
                 break;
         case 0x20:
-                data[3] |= 1 << 0;
+                _segment_on(CFG_REG_xx2xxx);
                 break;
         case 0x30:
-                data[4] |= 1 << 6;
+                _segment_on(CFG_REG_xx3xxx);
                 break;
         case 0x40:
-                data[3] |= 1 << 4;
+                _segment_on(CFG_REG_xx4xxx);
                 break;
         case 0x50:
-                data[3] |= 1 << 5;
+                _segment_on(CFG_REG_xx5xxx);
                 break;
         case 0x60:
-                data[4] |= 1 << 4;
+                _segment_on(CFG_REG_xx6xxx);
                 break;
         case 0x70:
-                data[4] |= 1 << 5;
+                _segment_on(CFG_REG_xx7xxx);
                 break;
         case 0x80:
-                data[4] |= 1 << 7;
+                _segment_on(CFG_REG_xx8xxx);
                 break;
         case 0x90:
-                data[3] |= 1 << 3;
+                _segment_on(CFG_REG_xx9xxx);
                 break;
         }
 }
@@ -272,76 +313,93 @@ void display_minutes(bcd2_t m)
  */
 void display_seconds(bcd2_t s)
 {
-        // Стирание секунд в массиве.
-        data[8] = 0;
-        data[7] &= 1 << 2; // Бит воскресенья не трогаем
-        data[6] &= ~(1 << 0 | 1 << 1 | 1 << 2 | 1 << 3 | 1 << 4);
+        /* Предварительное стирание сегментов. */
+        _segment_off(CFG_REG_xxxxx0);
+        _segment_off(CFG_REG_xxxxx1);
+        _segment_off(CFG_REG_xxxxx2);
+        _segment_off(CFG_REG_xxxxx3);
+        _segment_off(CFG_REG_xxxxx4);
+        _segment_off(CFG_REG_xxxxx5);
+        _segment_off(CFG_REG_xxxxx6);
+        _segment_off(CFG_REG_xxxxx7);
+        _segment_off(CFG_REG_xxxxx8);
+        _segment_off(CFG_REG_xxxxx9);
+        _segment_off(CFG_REG_xxxx0x);
+        _segment_off(CFG_REG_xxxx1x);
+        _segment_off(CFG_REG_xxxx2x);
+        _segment_off(CFG_REG_xxxx3x);
+        _segment_off(CFG_REG_xxxx4x);
+        _segment_off(CFG_REG_xxxx5x);
+        _segment_off(CFG_REG_xxxx6x);
+        _segment_off(CFG_REG_xxxx7x);
+        _segment_off(CFG_REG_xxxx8x);
+        _segment_off(CFG_REG_xxxx9x);
 
         // Единицы.
         switch (s & 0x0f) {
         case 0:
-                data[8] |= 1 << 6;
+                _segment_on(CFG_REG_xxxxx0);
                 break;
         case 1:
-                data[8] |= 1 << 5;
+                _segment_on(CFG_REG_xxxxx1);
                 break;
         case 2:
-                data[8] |= 1 << 4;
+                _segment_on(CFG_REG_xxxxx2);
                 break;
         case 3:
-                data[8] |= 1 << 2;
+                _segment_on(CFG_REG_xxxxx3);
                 break;
         case 4:
-                data[7] |= 1 << 0;
+                _segment_on(CFG_REG_xxxxx4);
                 break;
         case 5:
-                data[7] |= 1 << 1;
+                _segment_on(CFG_REG_xxxxx5);
                 break;
         case 6:
-                data[8] |= 1 << 0;
+                _segment_on(CFG_REG_xxxxx6);
                 break;
         case 7:
-                data[8] |= 1 << 1;
+                _segment_on(CFG_REG_xxxxx7);
                 break;
         case 8:
-                data[8] |= 1 << 3;
+                _segment_on(CFG_REG_xxxxx8);
                 break;
         case 9:
-                data[8] |= 1 << 7;
+                _segment_on(CFG_REG_xxxxx9);
                 break;
         }
 
         // Десятки.
         switch (s & 0xf0) {
         case 0x00:
-                data[6] |= 1 << 1;
+                _segment_on(CFG_REG_xxxx0x);
                 break;
         case 0x10:
-                data[6] |= 1 << 0;
+                _segment_on(CFG_REG_xxxx1x);
                 break;
         case 0x20:
-                data[7] |= 1 << 7;
+                _segment_on(CFG_REG_xxxx2x);
                 break;
         case 0x30:
-                data[7] |= 1 << 5;
+                _segment_on(CFG_REG_xxxx3x);
                 break;
         case 0x40:
-                data[6] |= 1 << 3;
+                _segment_on(CFG_REG_xxxx4x);
                 break;
         case 0x50:
-                data[6] |= 1 << 4;
+                _segment_on(CFG_REG_xxxx5x);
                 break;
         case 0x60:
-                data[7] |= 1 << 3;
+                _segment_on(CFG_REG_xxxx6x);
                 break;
         case 0x70:
-                data[7] |= 1 << 4;
+                _segment_on(CFG_REG_xxxx7x);
                 break;
         case 0x80:
-                data[7] |= 1 << 6;
+                _segment_on(CFG_REG_xxxx8x);
                 break;
         case 0x90:
-                data[6] |= 1 << 2;
+                _segment_on(CFG_REG_xxxx9x);
                 break;
         }
 }
@@ -358,26 +416,26 @@ void display_seconds(bcd2_t s)
 void display_dots(uint8_t d)
 {
         if (d & DISPLAY_DOT_RIGHT_BOTTOM) {
-                data[6] |= 1 << 6;
+                _segment_on(CFG_REG_RIGHT_BOTTOM);
         } else {
-                data[6] &= ~(1 << 6);
+                _segment_off(CFG_REG_RIGHT_BOTTOM);
         }
 
         if (d & DISPLAY_DOT_RIGHT_TOP) {
-                data[6] |= 1 << 7;
+                _segment_on(CFG_REG_RIGHT_TOP);
         } else {
-                data[6] &= ~(1 << 7);
+                _segment_off(CFG_REG_RIGHT_TOP);
         }
 
         if (d & DISPLAY_DOT_LEFT_BOTTOM) {
-                data[3] |= 1 << 7;
+                _segment_on(CFG_REG_LEFT_BOTTOM);
         } else {
-                data[3] &= ~(1 << 7);
+                _segment_off(CFG_REG_LEFT_BOTTOM);
         }
 
         if (d & DISPLAY_DOT_LEFT_TOP) {
-                data[2] |= 1 << 0;
+                _segment_on(CFG_REG_LEFT_TOP);
         } else {
-                data[2] &= ~(1 << 0);
+                _segment_off(CFG_REG_LEFT_TOP);
         }
 }
