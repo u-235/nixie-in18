def5fdd16233d23ef1f81881a56402b79d278b71
u-235 <nicegorov@yandex.ru>
2019-01-05 09:54:23 +0300

    Добавлен инкремент и декремент

--- src/bcd/bcd.c
+++ src/bcd/bcd.c
@@ -1,39 +1,69 @@
+/**
+ * @file
+ * @brief
+ * @details
+ *
+ * @date создан 17.10.2015
+ * @author Nick Egorrov
+ */
+
 #include "bcd.h"
 
-bcd2_t bcd_from_uint8(uint8_t tc)
+static bcd2_t adjust_add(bcd2_t a);
+
+static bcd2_t adjust_sub(bcd2_t s);
+
+extern bcd2_t bcd_from_uint8(uint8_t tc)
 {
         return (tc % 10) + ((tc / 10) << 4);
 }
 
-uint8_t bcd_to_uint8(bcd2_t tc)
+extern uint8_t bcd_to_uint8(bcd2_t tc)
 {
         return (tc & 0x0f) + ((tc >> 4) * 10);
 }
 
-bcd2_t bcd_add2(bcd2_t a, bcd2_t b)
+extern bcd2_t bcd2_inc(bcd2_t a)
 {
-        bcd2_t ret = a + b;
-        if ((ret & 0x0f) > 9) {
-                ret += 6;
-        }
-
-        if (ret >= 0xa0) {
-                ret += 0x60;
-        }
-
-        return ret;
+        return adjust_add(++a);
 }
 
-bcd2_t bcd_sub2(bcd2_t a, bcd2_t b)
+extern bcd2_t bcd2_dec(bcd2_t a)
 {
-        bcd2_t ret = a - b;
-        if ((ret & 0x0f) > 9) {
-                ret -= 6;
+        return adjust_sub(--a);
+}
+
+extern bcd2_t bcd_add2(bcd2_t a, bcd2_t b)
+{
+        return adjust_add(a + b);
+}
+
+extern bcd2_t bcd_sub2(bcd2_t a, bcd2_t b)
+{
+        return adjust_sub(a - b);
+}
+
+static bcd2_t adjust_add(bcd2_t a){
+        if ((a & 0x0f) > 9) {
+                a += 6;
         }
 
-        if (ret >= 0xa0) {
-                ret -= 0x60;
+        if (a >= 0xa0) {
+                a += 0x60;
         }
 
-        return ret;
+        return a;
+}
+
+static bcd2_t adjust_sub(bcd2_t s)
+{
+        if ((s & 0x0f) > 9) {
+                s -= 6;
+        }
+
+        if (s >= 0xa0) {
+                s -= 0x60;
+        }
+
+        return s;
 }
--- src/bcd/bcd.h
+++ src/bcd/bcd.h
@@ -3,6 +3,9 @@
  * @ingroup bcd
  * @brief Реализация BCD арифметики.
  * @details
+ *
+ * @date создан 17.10.2015
+ * @author Nick Egorrov
  */
 
 #ifndef BCD_H_
@@ -26,11 +29,17 @@ typedef uint8_t bcd2_t;
 extern "C" {
 #endif
 
-bcd2_t bcd_from_uint8(uint8_t tc);
-uint8_t bcd_to_uint8(bcd2_t tc);
+extern bcd2_t bcd_from_uint8(uint8_t tc);
 
-bcd2_t bcd_add2(bcd2_t a, bcd2_t b);
-bcd2_t bcd_sub2(bcd2_t a, bcd2_t b);
+extern uint8_t bcd_to_uint8(bcd2_t tc);
+
+extern bcd2_t bcd2_inc(bcd2_t a);
+
+extern bcd2_t bcd2_dec(bcd2_t a);
+
+extern bcd2_t bcd_add2(bcd2_t a, bcd2_t b);
+
+extern bcd2_t bcd_sub2(bcd2_t a, bcd2_t b);
 
 #ifdef __cplusplus
 }
